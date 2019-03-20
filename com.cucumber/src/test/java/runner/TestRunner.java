f074b87111f24e5dae73274fea33c6558b61062f
tskr4awesome <tskr4awesome@gmail.com>
2019-03-20 17:10:36 +0530

    Added comments

--- com.cucumber/src/test/java/runner/TestRunner.java
+++ com.cucumber/src/test/java/runner/TestRunner.java
@@ -3,7 +3,7 @@ package runner;
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-
+//commands
 @RunWith(Cucumber.class)
 @CucumberOptions(features={"features"},glue={"stepDefinition"})
 public class TestRunner
